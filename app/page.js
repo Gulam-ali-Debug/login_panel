85c02728f313b0fef64eb6bd47ab76cfa8438621
Gulam Ali Asgar Ansari <itzzzali4u@gmail.com>
2026-02-15 16:31:06 +0530

    main branch

--- app/page.js
+++ app/page.js
@@ -295,7 +295,7 @@ export default function Home() {
                         />
                       </div>
 
-                      {/* Google signup option */}
+                      {/* Google signup option this is */}
                       <div className="relative my-4">
                         <div className="absolute inset-0 flex items-center">
                           <div className="w-full border-t border-gray-300"></div>
